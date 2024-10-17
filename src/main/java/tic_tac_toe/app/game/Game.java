40834262d5161a655b469549fe0539049f342d8f
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-10-17 16:38:38 +0200

    refactor: rename uuid to mUuid

--- src/main/java/tic_tac_toe/app/game/Game.java
+++ src/main/java/tic_tac_toe/app/game/Game.java
@@ -3,20 +3,20 @@ package tic_tac_toe.app.game;
 import java.util.UUID;
 
 public class Game {
-    private UUID uuid;
+    private UUID mUuid;
 
     private String[][] mBoard;
 
     private int mWinner;
 
     public Game() {
-        this.uuid = UUID.randomUUID();
+        this.mUuid = UUID.randomUUID();
         this.mBoard = new String[][] { { "", "", "" }, { "", "", "" }, { "", "", "" } };
         this.mWinner = -1;
     }
 
     public UUID getId() {
-        return uuid;
+        return mUuid;
     }
 
     public String[][] getBoard() {
