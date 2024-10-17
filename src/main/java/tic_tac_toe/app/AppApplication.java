bab8dc1e286a296c4476a06d560ac7dc205b15bf
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-10-17 16:16:23 +0200

    chore: init repo

---
+++ src/main/java/tic_tac_toe/app/AppApplication.java
@@ -0,0 +1,13 @@
+package tic_tac_toe.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
