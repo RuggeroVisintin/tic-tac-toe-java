611ec24d0090b14fdb4fe969f6cba6f4a8283bd4
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-10-28 11:43:45 +0100

    refactor(api): implement post API

--- src/main/java/tic_tac_toe/app/application/game/controllers/GameController.java
+++ src/main/java/tic_tac_toe/app/application/game/controllers/GameController.java
@@ -1,13 +1,13 @@
 package tic_tac_toe.app.application.game.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import tic_tac_toe.app.application.game.useCases.NewGameUseCase;
 import tic_tac_toe.app.domain.game.models.Game;
 import tic_tac_toe.app.domain.game.ports.GameRepository;
 
-@Controller
+@RestController
 public class GameController {
 
     private final GameRepository repository;
