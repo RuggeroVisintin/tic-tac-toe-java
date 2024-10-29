7d948c95a24b2bf72710d7ae98e36bf2dc85a0fd
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-10-29 15:33:45 +0100

    refactor(application): add RetrieveExistingGameUseCase

---
+++ src/main/java/tic_tac_toe/app/application/game/useCases/RetrieveExistingGameUseCase.java
@@ -0,0 +1,12 @@
+package tic_tac_toe.app.application.game.useCases;
+
+import java.util.UUID;
+
+import tic_tac_toe.app.application.game.representation.GameRepresentation;
+import tic_tac_toe.app.domain.game.ports.GameRepository;
+
+public class RetrieveExistingGameUseCase {
+    public GameRepresentation execute(UUID gameId, GameRepository gameRepository) {
+        return new GameRepresentation(gameRepository.findById(gameId));
+    }
+}
---
+++ src/test/java/tic_tac_toe/app/application/game/useCases/RetrieveExistingGameUseCaseTests.java
@@ -0,0 +1,39 @@
+package tic_tac_toe.app.application.game.useCases;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+
+import tic_tac_toe.app.application.game.BaseFakeGameRepository;
+import tic_tac_toe.app.application.game.representation.GameRepresentation;
+import tic_tac_toe.app.domain.game.models.Game;
+
+public class RetrieveExistingGameUseCaseTests {
+    protected class FakeGameRepository extends BaseFakeGameRepository {
+        public Game savedGame;
+
+        @Override
+        public Game findById(UUID gameId) {
+            return savedGame;
+        }
+
+        @Override
+        public void save(Game game) {
+            savedGame = game;
+        }
+    }
+
+    @Test
+    void itShouldRetrieveAnExistingGameStatus() throws Exception {
+        FakeGameRepository gameRepository = new FakeGameRepository();
+        gameRepository.savedGame = new Game();
+        gameRepository.savedGame.nextMove(1, 1, 1);
+
+        GameRepresentation retrievedGame = (new RetrieveExistingGameUseCase().execute(gameRepository.savedGame.getId(),
+                gameRepository));
+
+        assertArrayEquals(new GameRepresentation(gameRepository.savedGame).getBoard(), retrievedGame.getBoard());
+    }
+}
