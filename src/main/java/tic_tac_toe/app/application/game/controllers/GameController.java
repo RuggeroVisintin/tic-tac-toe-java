de731a5de3739bc7df5e794a7b218dff19c5300f
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-10-29 15:15:23 +0100

    feat(application): add POST /v1/games/{gameId}/moves api
    Allows a player to do their move

--- src/main/java/tic_tac_toe/app/application/game/controllers/GameController.java
+++ src/main/java/tic_tac_toe/app/application/game/controllers/GameController.java
@@ -1,9 +1,15 @@
 package tic_tac_toe.app.application.game.controllers;
 
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import tic_tac_toe.app.application.game.representation.GameRepresentation;
+import tic_tac_toe.app.application.game.representation.MoveRepresentation;
+import tic_tac_toe.app.application.game.useCases.MakeAMoveUseCase;
 import tic_tac_toe.app.application.game.useCases.NewGameUseCase;
 import tic_tac_toe.app.domain.game.ports.GameRepository;
 
@@ -19,5 +25,12 @@ public class GameController {
     @PostMapping("/v1/games")
     public GameRepresentation newGame() {
         return new NewGameUseCase().execute(repository);
+    }
+
+    @PostMapping("/v1/games/{gameId}/moves")
+    public GameRepresentation makeAMove(@PathVariable UUID gameId, @RequestParam int playerId,
+            @RequestParam int x, @RequestParam int y) throws Exception {
+
+        return new MakeAMoveUseCase().execute(new MoveRepresentation(gameId, playerId, x, y), repository);
     }
 }
--- src/test/java/tic_tac_toe/app/application/game/controllers/GameControllerTests.java
+++ src/test/java/tic_tac_toe/app/application/game/controllers/GameControllerTests.java
@@ -1,5 +1,7 @@
 package tic_tac_toe.app.application.game.controllers;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Nested;
@@ -21,7 +23,7 @@ class FakeGameRepository implements GameRepository {
 
     @Override
     public Game findById(UUID gameId) {
-        return null;
+        return savedGame;
     }
 }
 
@@ -45,6 +47,22 @@ public class GameControllerTests {
 
             GameRepresentation newGame = controller.newGame();
             assertNotNull(newGame);
+        }
+    }
+
+    @Nested
+    class MakeAMoveTests {
+        @Test
+        void itShouldReturnTheUpdatedGame() throws Exception {
+            Game ongoingGame = new Game();
+            FakeGameRepository repository = new FakeGameRepository();
+            repository.savedGame = ongoingGame;
+
+            GameController controller = new GameController(repository);
+            GameRepresentation resultedGame = controller.makeAMove(ongoingGame.getId(), 1, 1, 1);
+
+            assertArrayEquals(new String[][] { { "O", "", "" }, { "", "", "" }, { "", "", "" } },
+                    resultedGame.getBoard());
         }
     }
 }
