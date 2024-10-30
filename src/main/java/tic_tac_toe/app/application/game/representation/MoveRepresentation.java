2f3b2c5a8b7d05ca81874487d2af79066a5c33e9
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-10-30 10:34:56 +0100

    fix(application): fix MoveRepresentation params order

--- src/main/java/tic_tac_toe/app/application/game/representation/MoveRepresentation.java
+++ src/main/java/tic_tac_toe/app/application/game/representation/MoveRepresentation.java
@@ -2,5 +2,5 @@ package tic_tac_toe.app.application.game.representation;
 
 import java.util.UUID;
 
-public record MoveRepresentation(UUID gameId, int x, int y, int playerId) {
+public record MoveRepresentation(UUID gameId, int playerId, int x, int y) {
 }
